d1aa57754e5a5b38d3656f46ef356caadc05b210
R3G155 <REGIVANFACULDADEETRABALHO@GMAIL.COM>
2025-06-25 11:00:02 -0300

    Add files via upload
    pagina web

---
+++ script.js
@@ -0,0 +1,5 @@
+let butao = document.querySelector("#butao")
+butao.addEventListener("click",ms)
+let txt = document.querySelector("#txt")
+function ms(){
+txt.innerHTML="Seja bem-vindo(a) à minha primeira página web &#128512";}
